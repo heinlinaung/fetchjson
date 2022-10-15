e950bd3cc26784adb5944e3b8a69809bea4b5ee6
Hein Lin Aung <heinlinaung6891@gmail.com>
2022-10-15 19:38:40 +0630

    Typescript variables with type annotation and type inference

--- features/variables.ts
+++ features/variables.ts
@@ -1,6 +1,12 @@
 let apples: number = 5;
 let speed: string = 'slow'
 let hasName: boolean = true
+/* # type inference version
+# typescript decide the type on variable declration
+let apples= 5;
+let speed= 'slow'
+let hasName= true
+*/
 
 let nothingNull: null = null
 let nothingUndefined: undefined = undefined
@@ -24,3 +30,34 @@ let person: { name: string; age: number } = {
   name: 'hein',
   age: 31
 }
+
+// Function
+// -------------- input ------- return value ----------
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i)
+  return true
+}
+
+// when to use type annotation
+// 1) function that returns 'any' type value
+const json = '{"x":10,"y":20}'
+const coordinates: { x: number; y: number } = JSON.parse(json)
+// coordinates.blah // raise err
+
+// 2) when declare a variable and assign value later
+let words = ['red', 'green', 'brown']
+let foundWord: boolean;
+words.forEach(x => {
+  if (x === 'red') {
+    foundWord = true
+  }
+})
+
+// 3) variable whose type cannot be inferred correctly
+let numbers2 = [-10, -1, 20]
+let numberAboveZero: boolean | number = false
+numbers2.forEach(x => {
+  if (x > 0) {
+    numberAboveZero = x
+  }
+})
