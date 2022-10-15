da0a6e94ffc9d63ceca92f58fc64a009125cb43b
Hein Lin Aung <heinlinaung6891@gmail.com>
2022-10-15 16:27:05 +0630

    setting variables with type annotations

---
+++ features/variables.ts
@@ -0,0 +1,26 @@
+let apples: number = 5;
+let speed: string = 'slow'
+let hasName: boolean = true
+
+let nothingNull: null = null
+let nothingUndefined: undefined = undefined
+
+// built-in objects
+let now: Date = new Date()
+
+// Array
+let colors: string[] = ['red']
+let numbers: number[] = [12, 3, 2]
+let booleanValues: boolean[] = [true, true, false]
+
+// Class
+class Car {
+
+}
+let car: Car = new Car()
+
+// object literal
+let person: { name: string; age: number } = {
+  name: 'hein',
+  age: 31
+}
