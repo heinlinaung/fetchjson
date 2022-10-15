aa7cb70d333bde340d486631a6c278d341584b98
Hein Lin Aung <heinlinaung6891@gmail.com>
2022-10-15 12:35:27 +0630

    Restricting response json with interface

--- index.ts
+++ index.ts
@@ -1,6 +1,21 @@
 import axios from 'axios'
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 axios.get(url).then(res => {
-  console.log(res.data)
+  const todo = res.data as Todo
+
+  const id = todo.id
+  const title = todo.title
+  const completed = todo.completed
+
+  console.log(`
+  Todo id: ${id}
+  with title of ${title}
+  and finished status: ${completed}
+  `)
 })
