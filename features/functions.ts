b3a4dae8e7bc966a12b1983e91c5cb7fe36572df
Hein Lin Aung <heinlinaung6891@gmail.com>
2022-10-17 11:12:31 +0630

    Type annotation with functions [void , never]

--- features/functions.ts
+++ features/functions.ts
@@ -24,3 +24,28 @@ function divide(a: number, b: number): number {
 const multiply = (a: number, b: number): Number => {
   return a * b
 }
+
+// return type:void -> function that returns nothing at all (not even null, undefined)
+const logger = (message: string): void => {
+  console.log(message)
+  // return null // raise error
+}
+
+// [never] : when functino will never reach the end of function [rare case, will never use it]
+const throwError = (message: string): never => {
+  throw new Error(message)
+
+  /* why?
+  if(!message){
+    throw new Error(message)
+  }
+  return message
+  // in this case return type must be 'string'
+   */
+  /* why2?
+  if(!message){
+    throw new Error(message)
+  }
+  // in this case return type must be 'void'
+   */
+}
